5a446e6dd479d7a0d2fc78d9a5556d20e2fdd6d3
michrome <m+nitrous@j4e.name>
2016-06-10 17:48:20 +0000

    Hello World

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http')
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hello World\n')
+})
+var port = process.env.PORT || 5000
+server.listen(port, '0.0.0.0')
+
