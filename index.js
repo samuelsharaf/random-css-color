7da2bf4ddbee8b6ab8dbc30f699a2e60ddf1dbce
michrome <m+nitrous@j4e.name>
2016-06-24 20:39:48 +0000

    Bind to HEROKU_PRIVATE_IP if it exists

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var server = http.createServer(function (request, response) {
   response.end('Hello World\n')
 })
 var port = process.env.SERVICE_PORT || process.env.PORT || 5000
-server.listen(port, '0.0.0.0')
+var ip = process.env.HEROKU_PRIVATE_IP || '0.0.0.0'
+server.listen(port, ip)
 
