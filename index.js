26768cb410b218d09361aa0058da505cb50541be
michrome <m+nitrous@j4e.name>
2016-06-24 20:28:20 +0000

    Bind to SERVICE_PORT if one exists

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.end('Hello World\n')
 })
-var port = process.env.PORT || 5000
+var port = process.env.SERVICE_PORT || process.env.PORT || 5000
 server.listen(port, '0.0.0.0')
 
