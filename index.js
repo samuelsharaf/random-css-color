707fe043bc2abbab581b1c82da4f3f77cecc7605
michrome <m+nitrous@j4e.name>
2016-06-24 21:09:05 +0000

    Return a random css color

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var http = require('http')
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  response.end(randomColor() + '\n')
 })
 var port = process.env.SERVICE_PORT || process.env.PORT || 5000
 var ip = process.env.HEROKU_PRIVATE_IP || '0.0.0.0'
 server.listen(port, ip)
 
+function randomColor () {
+  var cssColors = ['black', 'red', 'purple', 'green']
+  return cssColors[Math.floor(Math.random() * cssColors.length)]
+}
